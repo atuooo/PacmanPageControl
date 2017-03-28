b541867525c46a2bf1c9b98d06ec5e2a37cf90f5
ooatuoo <aaatuooo@gmail.com>
2017-03-28 13:01:46 +0800

    support Carthage

---
+++ PacmanPageControl-Demo/PacmanPageControl/PacmanPageControl.h
@@ -0,0 +1,19 @@
+//
+//  PacmanPageControl.h
+//  PacmanPageControl
+//
+//  Created by ooatuoo on 2017/3/28.
+//  Copyright © 2017年 ooatuoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PacmanPageControl.
+FOUNDATION_EXPORT double PacmanPageControlVersionNumber;
+
+//! Project version string for PacmanPageControl.
+FOUNDATION_EXPORT const unsigned char PacmanPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PacmanPageControl/PublicHeader.h>
+
+
